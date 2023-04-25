7d4b9d279bbba5b04bed2c863b9e130bbc7dc65b
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-25 07:59:39 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Poll Vote Web Application
+
+This is a poll vote web application that allows users to create and vote on polls. The application uses HTML, CSS, and JavaScript for the frontend and Node.js for the backend.
+
+## Features
+
+- Create polls with multiple options
+- Vote on existing polls
+- See the results of polls
+
+## Getting Started
+
+1. Clone the repository:
+
+` git clone https://github.com/abubalo/poll-vote.git `
+
+
+2. Start the server:
+
+
+3. Open the application in your browser:
+http://localhost:3000/
+
---
+++ clients/main.js
@@ -0,0 +1,26 @@
+class Poll{
+    constructor(root, title){
+        this.root = root;
+        this.selected = sessionStorage.getItem("poll-selected");
+        this.endpoint = "http://localhost:5502/poll";
+        
+        this.root.insertAdjacentHTML('afterbegin', String.raw`
+            <div class="poll__title">${title}</div>
+        `)
+
+        this._refresh()
+
+}
+    async _refresh(){
+        const response = await fetch(this.endpoint);
+        const data = await response.json();
+
+        console.log(data)
+    }
+}
+
+const p = new Poll(
+    document.querySelector('.poll'),
+    "Which do you prefer?"
+)
+console.log(p)
